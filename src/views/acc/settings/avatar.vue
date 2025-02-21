9f232cee74e6f41dff2d62fbd5c30b999c8abf76
=Babak Alizadeh <alizadeh.babak@gmail.com>
2025-02-21 15:30:00 -0800

    add seal to businesses

--- src/views/acc/settings/avatar.vue
+++ src/views/acc/settings/avatar.vue
@@ -1,67 +1,96 @@
 <template>
-  <div class="block block-content-full ">
-    <div id="fixed-header" class="block-header block-header-default bg-gray-light pt-2 pb-1">
-      <h3 class="block-title text-primary-dark">
-        <button @click="$router.back()" type="button"
-          class="float-start d-none d-sm-none d-md-block btn btn-sm btn-link text-warning">
-          <i class="fa fw-bold fa-arrow-right"></i>
-        </button>
-        <i class="fa-solid fa-user-tie"></i>
-        نمایه کسب‌و‌کار
-      </h3>
-      <div class="block-options">
+  <v-toolbar color="toolbar" :title="$t('drawer.avatar_settings')">
+    <template v-slot:prepend>
+      <v-tooltip :text="$t('dialog.back')" location="bottom">
+        <template v-slot:activator="{ props }">
+          <v-btn v-bind="props" @click="$router.back()" class="d-none d-sm-flex" variant="text"
+            icon="mdi-arrow-right" />
+        </template>
+      </v-tooltip>
+    </template>
+    <v-spacer></v-spacer>
+  </v-toolbar>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="4" md="4">
+        <v-card :title="$t('dialog.avatar_now')">
+          <v-card-text class="text-center">
+            <img class="img-fluid" style="max-width: 10rem;" :src="getImgSrc()" />
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="8" md="8">
+        <v-card :title="$t('dialog.avatar')">
+          <v-card-text>
+            <ul class="text-primary">
+              <li>
+                نمایه یک تصویر است که در بالای فاکتورها و اسناد حسابداری قرار می گیرد.
+              </li>
+              <li>
+                بهترین نمایش تصویر در لوگو و آرم‌های مربعی است
+              </li>
+              <li>
+                حداکثر سایز 512 در 512 پیکسل است و بیشینه حجم فایل انتخابی نباید از ۱ مگابایت تجاوز کند.
+              </li>
+              <li>
+                فرمت فایل قابل قبول jpg , jpeg , png است.
+              </li>
+            </ul>
+            <form @submit.prevent="save()">
+              <div class="input-group">
+                <input type="file" required class="form-control" v-on:change="selectedFile($event)" id="avatarImgFile"
+                  aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+                <button :disabled="loading" class="btn btn-success" type="submit" id="inputGroupFileAddon04">
+                  <i class="fa-solid fa-floppy-disk me-1"></i>
+                  ذخیره نمایه
+                </button>
+              </div>
+            </form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="4" md="4">
+        <v-card :title="$t('dialog.seal_now')">
+          <v-card-text class="text-center">
+            <img class="img-fluid" style="max-width: 10rem;" :src="getSealImgSrc()" />
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="8" md="8">
+        <v-card :title="$t('dialog.seal')">
+          <v-card-text>
+            <ul class="text-primary">
+              <li>
+                مهر کسب و کار در پایین اسناد حسابداری از جمله فاکتور های فروش و ... درج می‌شود 
+              </li>
+              <li>
+                بهترین نتیجه ممکن در مهر های با پس زمینه سفید رنگ یا بدون رنگ است.
+              </li>
+              <li>
+                حداکثر سایز 512 در 512 پیکسل است و بیشینه حجم فایل انتخابی نباید از ۱ مگابایت تجاوز کند.
+              </li>
+              <li>
+                فرمت فایل قابل قبول jpg , jpeg , png است.
+              </li>
+            </ul>
+            <form @submit.prevent="seal_save()">
+              <div class="input-group">
+                <input type="file" required class="form-control" v-on:change="seal_selectedFile($event)" id="sealImgFile"
+                  aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+                <button :disabled="loading" class="btn btn-success" type="submit" id="inputGroupFileAddon04">
+                  <i class="fa-solid fa-floppy-disk me-1"></i>
+                  ذخیره مهر
+                </button>
+              </div>
+            </form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 
-      </div>
-    </div>
-    <div class="block-content">
-      <loading color="blue" loader="dots" v-model:active="loading" :is-full-page="false"/>
-      <div class="row">
-        <div class="col-sm-12 col-md-12 my-3">
-          <div class="row">
-            <div class="col-sm-12 col-md-4 mt-1">
-              <div class="card">
-                <div class="card-header">
-                  نمایه کنونی
-                </div>
-                <div class="card-body">
-                  <img class="img-fluid" :src="getImgSrc()" />
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-12 col-md-8 mt-1">
-              <div class="alert alert-success" role="alert">
-                <h4 class="alert-heading"></h4>
-                <ul>
-                  <li>
-                    نمایه یک تصویر است که در بالای فاکتورها و اسناد حسابداری قرار می گیرد.
-                  </li>
-                  <li>
-                    بهترین نمایش تصویر در لوگو و آرم‌های مربعی است
-                  </li>
-                  <li>
-                    حداکثر سایز 512 در 512 پیکسل است و بیشینه حجم فایل انتخابی نباید از ۱ مگابایت تجاوز کند.
-                  </li>
-                  <li>
-                    فرمت فایل قابل قبول jpg , jpeg , png است.
-                  </li>
-                </ul>
-              </div>
-              <form @submit.prevent="save()">
-                <div class="input-group">
-                  <input type="file" required class="form-control" v-on:change="selectedFile($event)" id="avatarImgFile" aria-describedby="inputGroupFileAddon04"
-                    aria-label="Upload">
-                  <button :disabled="loading" class="btn btn-success" type="submit" id="inputGroupFileAddon04">
-                    <i class="fa-solid fa-floppy-disk me-1"></i>
-                    ذخیره
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
@@ -71,6 +100,7 @@ import Swal from "sweetalert2";
 import { ref } from "vue";
 import { getApiUrl, getSiteName } from "@/hesabixConfig"
 import Loading from "vue-loading-overlay";
+import { DialogHandler } from "maz-ui";
 
 export default {
   name: "avatar",
@@ -81,7 +111,9 @@ export default {
     return {
       loading: true,
       file: null,
-      lastImg: 'default.png'
+      lastImg: 'default.png',
+      seal_file: null,
+      seal_lastImg: 'default.png'
     }
   },
   methods: {
@@ -90,6 +122,12 @@ export default {
         .then((response) => {
           this.loading = false;
           this.lastImg = response.data;
+        })
+
+        axios.get('/api/seal/get')
+        .then((response) => {
+          this.loading = false;
+          this.seal_lastImg = response.data;
         })
     },
     save() {
@@ -136,11 +174,64 @@ export default {
         }
       });
     },
+    seal_save() {
+      this.loading = true;
+      const formData = new FormData();
+      console.log(this.seal_file)
+      formData.append('bytes', this.seal_file);
+      axios.post('/api/seal/post',
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        }
+      ).then((response) => {
+        this.loading = false;
+        if (response.data == 'e') {
+          Swal.fire({
+            text: 'فرمت فایل اشتباه است.',
+            icon: 'warning',
+            confirmButtonText: 'قبول'
+          })
+        }
+        else if (response.data == 's') {
+          Swal.fire({
+            text: 'حجم فایل ارسال باید کمتر از یک مگابایت باشد.',
+            icon: 'warning',
+            confirmButtonText: 'قبول'
+          })
+        }
+        else if (response.data == 'is') {
+          Swal.fire({
+            text: 'سایز تصویر نامناسب است و طول و عرض آن باید کمتر از 512 پیکسل باشد.',
+            icon: 'warning',
+            confirmButtonText: 'قبول'
+          })
+        }
+        else {
+          Swal.fire({
+            text: 'مهر کسب‌و‌کار با موفقیت تغییر یافت.',
+            icon: 'success',
+            confirmButtonText: 'قبول'
+          });
+          this.loadData();
+        }
+      });
+    },
     getImgSrc() {
       return getApiUrl() + '/api/avatar/get/file/' + this.lastImg;
     },
     selectedFile(event) {
       this.file = event.target.files[0]
+    },
+
+    getSealImgSrc() {
+      return getApiUrl() + '/api/seal/get/file/' + this.seal_lastImg;
+    },
+
+    seal_selectedFile(event) {
+      this.seal_file = event.target.files[0]
     },
   },
   beforeMount() {
